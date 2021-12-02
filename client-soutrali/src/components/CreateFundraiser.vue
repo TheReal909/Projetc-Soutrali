e17e38c117f7e90a6b725d5426ce72484de8f744
thereal909 <hadybah@outlook.com>
2021-12-02 03:41:45 -0700

    update on view

--- client-soutrali/src/components/CreateFundraiser.vue
+++ client-soutrali/src/components/CreateFundraiser.vue
@@ -1,22 +1,20 @@
 <template>
   <v-main>
-      <v-card-title class="text-center justify-center py-6">
-        <h1 class="font-weight-bold text-h2 light-green--text">
-          Create your fundraiser in just a few steps !
-        </h1>
-      </v-card-title>
-
+    <v-card-title class="text-center justify-center py-6">
+      <h1 class="font-weight-bold text-h2 light-green--text">
+        Create your fundraiser in just a few steps !
+      </h1>
+    </v-card-title>
+    
+  <!-- should responsive the page -->
+    <div class="stepperLayout">
       <v-stepper v-model="e1">
         <v-stepper-header>
-          <v-stepper-step :complete="e1 > 1" step="1">
-            Step 1
-          </v-stepper-step>
+          <v-stepper-step :complete="e1 > 1" step="1"> Step 1 </v-stepper-step>
 
           <v-divider></v-divider>
 
-          <v-stepper-step :complete="e1 > 2" step="2">
-            Step 2
-          </v-stepper-step>
+          <v-stepper-step :complete="e1 > 2" step="2"> Step 2 </v-stepper-step>
 
           <v-divider></v-divider>
 
@@ -24,16 +22,23 @@
         </v-stepper-header>
 
         <v-stepper-items>
-          <v-stepper-content step="1">
-            <v-card
-              class="mb-12"
-              color="grey lighten-1"
-              height="200px"
-            ></v-card>
+          <div class="container">
+            <v-stepper-content step="1">
+              <v-form ref="form" lazy-validation>
+                <v-text-field label="Name" required></v-text-field>
 
-           <v-btn color="light-green" @click="e1 = 2"> Continue </v-btn>
-          </v-stepper-content>
+                <v-text-field label="E-mail" required></v-text-field>
 
+                <v-select label="Item" required></v-select>
+
+                <v-text-field label="E-mail2" required></v-text-field>
+
+                <v-text-field label="E-mail3" required></v-text-field>
+              </v-form>
+
+              <v-btn color="light-green" @click="e1 = 2"> Continue </v-btn>
+            </v-stepper-content>
+          </div>
           <v-stepper-content step="2">
             <v-card
               class="mb-12"
@@ -55,11 +60,11 @@
 
             <v-btn color="light-green" @click="e1 = 1"> Submit ? </v-btn>
 
-            <v-btn color="primary" @click="el = 2" text> Previous </v-btn>
+            <v-btn color="primary" @click="e1 = 2" text> Previous </v-btn>
           </v-stepper-content>
         </v-stepper-items>
       </v-stepper>
-
+    </div>
   </v-main>
 </template>
 
@@ -73,4 +78,17 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 600px;
+}
+
+.stepperLayout {
+  margin-left: auto;
+  margin-right: auto;
+  width: 800px;
+}
+</style>
---
+++ client-soutrali/src/components/FundraiserType.vue
@@ -0,0 +1,25 @@
+<template>
+    <main>
+        <header><h1>Hi ! Tell us who are you fundraising for :) </h1></header>
+        <div class="buttons">
+            <section class="forMeBtn">
+                    <v-btn small elevation="" color="">Button 2</v-btn> 
+            </section>
+            <section class="forElseBtn">
+                    <v-btn small elevation="" color="">Button 1</v-btn> 
+            </section>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .buttons{
+        display: block;
+    }
+</style>
